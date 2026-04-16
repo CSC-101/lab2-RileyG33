68a8a1792543a22dab89db09862ef37d9f696062
riley <Rgumpel@calpoly.edu>
2026-04-16 14:48:00 -0700

    Lab commit 1

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# Create a welcome message.
-# Input: a name as a string
-# Result: a string
-def welcome_message(name:str) -> str:
-   message = "Hello, " + name + "."
+def smallest(n:float, m:float) -> float:
+    if n < m:
+        return n             # For which calls below is this statement evaluated?
+    else:
+        return m
 
-   return message
-
-
-message = welcome_message("Rgumpel@calpoly.edu")
-print(message)
+first = smallest(3, 2)       # What is the value of first?
+second = smallest(2, 2)      # What is the value of second? Is this a reasonable result? Why or why not?
+print(first)
